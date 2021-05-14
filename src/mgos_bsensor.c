12892b72bec17e3640a9fec57d2d3470686ef970
fbalestrini <gaojing@hotmail.it>
2021-05-14 16:44:49 +0200

    Updated

--- src/mgos_bsensor.c
+++ src/mgos_bsensor.c
@@ -40,7 +40,7 @@ mgos_bsensor_t mgos_bsensor_create(const char *id, enum mgos_bthing_notify_state
       LOG(LL_INFO, ("bSensor '%s' successfully created.", id));
       return sens;
     }
-    mg_bthing_reset(MG_BTHING_SENS_CAST3(sens);
+    mg_bthing_reset(MG_BTHING_SENS_CAST3(sens));
   }
   free(sens);
   return NULL; 
