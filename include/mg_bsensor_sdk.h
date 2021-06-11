773958d047f5b0abf230d5bc7355c3a2828fb484
fbalestrini <gaojing@hotmail.it>
2021-06-11 07:32:38 +0200

    Updated

--- include/mg_bsensor_sdk.h
+++ include/mg_bsensor_sdk.h
@@ -65,8 +65,6 @@ bool mg_bsensor_init(mgos_bsensor_t sens, struct mg_bsensor_cfg *cfg);
 
 void mg_bsensor_reset(mgos_bsensor_t sens);
 
-bool mg_bsensor_update_state(mgos_bsensor_t sens);
-
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
--- src/mgos_bsensor.c
+++ src/mgos_bsensor.c
@@ -11,7 +11,7 @@ mgos_bthing_t MGOS_BSENSOR_THINGCAST(mgos_bsensor_t sensor) {
 }
 
 static void mg_bsensor_poll_cb(void *sens) {
-  if (!mg_bsensor_update_state((mgos_bsensor_t)sens)) {
+  if (!mg_bthing_update_state((mgos_bsensor_t)sens)) {
     LOG(LL_DEBUG, ("Error retrieving the state of bSensor '%s' during the polling callback.",
       mgos_bthing_get_id(MGOS_BSENSOR_THINGCAST((mgos_bsensor_t)sens))));
   }
@@ -22,7 +22,7 @@ static void mg_bsensor_int_cb(int pin, void *sens) {
     struct mg_bsensor_cfg *cfg = MG_BSENSOR_CFG(sens);
     if (cfg->int_cfg.pin == pin) {
       cfg->int_cfg.triggered = 1;
-      if (!mg_bsensor_update_state((mgos_bsensor_t)sens)) {
+      if (!mg_bthing_update_state((mgos_bsensor_t)sens)) {
         LOG(LL_DEBUG, ("Error retrieving the state of bSensor '%s' during the interrupt callback.",
           mgos_bthing_get_id(MGOS_BSENSOR_THINGCAST((mgos_bsensor_t)sens))));
       }
